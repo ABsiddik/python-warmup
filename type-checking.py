80502039d29682e1b9314f8fc084043be73523d4
absiddik <absiddika@gmail.com>
2025-09-23 00:50:42 +0600

    Built-in type checking

---
+++ type-checking.py
@@ -0,0 +1,16 @@
+print("Here are some of the common built-in types")
+
+print(type("welcome"))        # str (string)
+print(type(123))              # int (integer)
+print(type(12.5))             # float (floating point number)
+print(type(3+4j))             # complex (complex number)
+print(type(True))             # bool (boolean)
+print(type([1, 2, 3]))        # list
+print(type((1, 2, 3)))        # tuple
+print(type({1, 2, 3}))        # set
+print(type({"a": 1, "b": 2})) # dict
+print(type(None))             # NoneType
+print(type(b"hello"))         # bytes
+print(type(bytearray(5)))     # bytearray
+print(type(range(5)))         # range
+print(type(frozenset([1,2]))) # frozenset
